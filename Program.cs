71265a3d1817982220e2c9c1cabf588018bf33ec
Matt Huber <huber.matt@gmail.com>
2019-06-10 21:12:00 -0400

    added solution

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Hubes.Kata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(@" _   _       _                 _   __      _        ");
+            Console.WriteLine(@"| | | |     | |               | | / /     | |       ");
+            Console.WriteLine(@"| |_| |_   _| |__   ___  ___  | |/ /  __ _| |_ __ _ ");
+            Console.WriteLine(@"|  _  | | | | '_ \ / _ \/ __| |    \ / _` | __/ _` |");
+            Console.WriteLine(@"| | | | |_| | |_) |  __/\__ \_| |\  \ (_| | || (_| |");
+            Console.WriteLine(@"\_| |_/\__,_|_.__/ \___||___(_)_| \_/\__,_|\__\__,_|");
+            
+            
+            
+            Console.WriteLine(@"                _ ");
+            Console.WriteLine(@"               | |");
+            Console.WriteLine(@"  ___ _ __   __| |");
+            Console.WriteLine(@" / _ \ '_ \ / _` |");
+            Console.WriteLine(@"|  __/ | | | (_| |");
+            Console.WriteLine(@" \___|_| |_|\__,_|");
+            Console.ReadLine();
+        }
+    }
+}
